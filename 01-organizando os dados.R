48f560bdff75f82beafc99f6b65ba9d1603a1296
Fernanda <fernandababarros@hotmail.com>
2018-05-16 14:48:16 -0300

    organizing data

---
+++ 01-organizando os dados.R
@@ -0,0 +1,22 @@
+#01-organizando os dados
+
+#carregando os pacotes
+library(mice)
+library(VIM)
+library(CASdatasets)
+
+#carregando o banco de dados
+data(ustermlife)
+head(ustermlife)
+dados_originais = subset(ustermlife, select = c(Gender, Age, MarStat,
+                                                Education, Ethnicity, Income))
+
+#gerando NA's na variável Income
+#e criando um novo banco de dados com os valores faltantes
+set.seed(0)
+dados = subset(ustermlife, select = c(Gender, Age, MarStat, Education,
+                                      Ethnicity, Income))
+random = rbinom(length(dados$Income), size = 1, prob=0.2)
+dados[,6] = ifelse(random, NA , dados$Income)
+
+save.image(file = "01-organizando os dados.RData")
