dabc75eaf16b738d0f5374f842ed3617c949d1bc
Fernanda <fernandababarros@github.com>
2018-05-20 17:34:32 -0300

    descriptive analysis
    include boxplots

--- 02-análise descritiva.R
+++ 02-análise descritiva.R
@@ -14,30 +14,41 @@ attach(dados_originais)
 #visão geral das variáveis
 summary(dados_originais)
 
-hist(log(Income), col="grey", probability=TRUE, xlab="Renda",
-     main="Histograma da Renda")
+hist(log(Income), col="grey", probability=TRUE, xlab="log(Renda)",
+     main="Histograma da log(Renda)")
 lines(density(log(Income)), col="blue", lwd=2)
 
-# densityplot(log(Income), col="black", xlab="Renda")
+# densityplot(log(Income), col="black", xlab="log(Renda)")
 
 #diferença entre os valores de Renda para homens e mulheres
 #hipótese homens ganham mais do que mulheres?
 table(Gender)
 boxplot(log(Income) ~ Gender, col="grey",
-        xlab="Gênero", main="Boxplot da variável Renda e Gênero")
+        xlab="Gênero", main="Boxplot da variável log(Renda) e Gênero")
 
 #quanto maior os anos de escolaridade maior a renda?
-boxplot(log(Income) ~ Education, col="#636363",
-      main="Boxplot da variável Renda e Anos de Escolaridade",
+boxplot(log(Income) ~ Education, col="grey",
+      main="Boxplot da variável log(Renda) e Anos de Escolaridade",
       xlab="Anos de Escolaridade")
 abline(h=mean(log(Income)), lty=2)
 
 #avaliar se o estado civil influencia na renda dos entrevistados
 table(MarStat)
-boxplot(log(Income) ~ MarStat, col="#636363",
-        main="Boxplot da variável Renda e o Estado civil",
+boxplot(log(Income) ~ MarStat, col="grey",
+        main="Boxplot da variável log(Renda) e o Estado civil",
         xlab="Estado Civil")
 #0=outros, 1=casado(a), 2=morando com o parceiro(a)
+
+#avaliar a etnia com a renda
+table(Ethnicity)
+boxplot(log(Income) ~ Ethnicity, col="grey",
+        main="Boxplot da variável log(Renda) e a Etnia",
+        xlab="Etnia")
+#1=, 2=, 3=, 7=
+
+#gráfico de dispersão da renda pela idade com o gênero
+plot(log(Income), Age, xlab="log(Renda)", ylab="Idade",
+     main="Gráfico da log(Renda) x Idade")
 
 detach(dados_originais)
 
