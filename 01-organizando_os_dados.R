afed2bfcb3c9f374090a51ae8f27bf4a7c0f669d
Fernanda <fernandababarros@github.com>
2018-08-22 15:46:00 -0300

    imputação

--- 01-organizando_os_dados.R
+++ 01-organizando_os_dados.R
@@ -23,7 +23,7 @@ set.seed(0)
 dados_MCAR = subset(ustermlife, select = c(Gender, Age, MarStat, Education,
                                       Ethnicity, Income))
 random_MCAR = rbinom(length(dados_MCAR$Income), size = 1, prob=0.2)
-dados_MCAR[,6] = ifelse(random_MCAR, NA, dados$Income)
+dados_MCAR[,6] = ifelse(random_MCAR, NA, dados_MCAR$Income)
 
 
 source(file="02-analise_descritiva.R", encoding="UTF-8")
@@ -33,27 +33,27 @@ set.seed(0)
 dados_MAR = dados_originais
 dados_MAR_fem = dados_MAR[dados_originais$Gender=='Feminino',]
 random_MAR_fem = rbinom(length(dados_MAR_fem$Income), size = 1, prob=0.1)
-dados_MAR_fem[,7] = ifelse(random_MAR_fem, NA, dados$Income)
+dados_MAR_fem[,7] = ifelse(random_MAR_fem, NA, dados_MAR_fem$Income)
 
-#gênero feminino
+#gênero masculino
 dados_MAR_masc = dados_MAR[dados_originais$Gender=='Masculino',]
 random_MAR_masc = rbinom(length(dados_MAR_masc$Income), size = 1, prob=0.3)
-dados_MAR_masc[,7] = ifelse(random_MAR_masc, NA, dados$Income)
+dados_MAR_masc[,7] = ifelse(random_MAR_masc, NA, dados_MAR_masc$Income)
 
 #ensino fundamental
 dados_MAR_ef = dados_MAR[dados_originais$Education2=='Ensino Fundamental',]
 random_MAR_ef = rbinom(length(dados_MAR_ef$Income), size = 1, prob=0.05)
-dados_MAR_ef[,7] = ifelse(random_MAR_ef, NA, dados$Income)
+dados_MAR_ef[,7] = ifelse(random_MAR_ef, NA, dados_MAR_ef$Income)
 
 #ensino médio
 dados_MAR_em = dados_MAR[dados_originais$Education2=='Ensino Médio',]
 random_MAR_em = rbinom(length(dados_MAR_em$Income), size = 1, prob=0.2)
-dados_MAR_em[,7] = ifelse(random_MAR_em, NA, dados$Income)
+dados_MAR_em[,7] = ifelse(random_MAR_em, NA, dados_MAR_em$Income)
 
 #ensino superior
 dados_MAR_es = dados_MAR[dados_originais$Education2=='Ensino Superior',]
 random_MAR_es = rbinom(length(dados_MAR_es$Income), size = 1, prob=0.4)
-dados_MAR_es[,7] = ifelse(random_MAR_es, NA, dados$Income)
+dados_MAR_es[,7] = ifelse(random_MAR_es, NA, dados_MAR_es$Income)
 
 #banco completo com os missings na renda pelas probabilidades de feminino
 #e de masculino
